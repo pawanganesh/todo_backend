67c608b3902e119b85d0142ba0ccde1091e78a9e
pawanlalganesh <pawanlalganesh@gmail.com>
2022-06-02 14:04:29 +0545

    added more test for user app

--- user/tests/test_views.py
+++ user/tests/test_views.py
@@ -22,4 +22,16 @@ class TestViews(TestSetup):
         response = self.client.get(self.current_user_url, HTTP_AUTHORIZATION='Bearer ' + token)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['email'], self.register_data['email'])
-        
+
+    def test_user_profile_retrieve_update_api_view_with_invalid_token(self):
+        register_response = self.client.post(self.register_url, self.register_data, format='json')
+        self.assertEqual(register_response.status_code, 201)
+        login_response = self.client.post(self.login_url, self.login_data, format='json')
+        self.assertEqual(login_response.status_code, 200)
+        token = login_response.data['access_token']
+        response = self.client.get(self.current_user_url, HTTP_AUTHORIZATION='Bearer ' + token + 'invalid')
+        self.assertEqual(response.status_code, 403)
+
+    def test_user_profile_retrieve_update_api_view_without_token(self):
+        response = self.client.get(self.current_user_url)
+        self.assertEqual(response.status_code, 403)
